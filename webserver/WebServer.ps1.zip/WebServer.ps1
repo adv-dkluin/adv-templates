f1961ed828ecf8e7611cf5e3d44d02f71368e497
adv-dkluin <dkluin@advisor.nl>
2019-06-27 15:36:04 +0200

    Added DSC

---
+++ webserver/WebServer.ps1.zip/WebServer.ps1
@@ -0,0 +1,13 @@
+﻿configuration WebServer
+{
+    Node IsWebServer
+    {
+        WindowsFeature IIS
+        {
+            Ensure               = 'Present'
+            Name                 = 'Web-Server'
+            IncludeAllSubFeature = $true
+        }
+    }
+
+}
